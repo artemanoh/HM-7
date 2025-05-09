bea75aa8614612d6c8580dd228418806a4fb2040
artemanoh <61391756+artemanoh@users.noreply.github.com>
2025-05-09 16:11:01 +0300

    make hm handlebars

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { products } from "./data.js";
 import createList from "./products/products.hbs";
+import { products } from "./data.js";
 
 const body = document.querySelector("body");
 const productListHTML = createList({products});
